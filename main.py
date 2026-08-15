0c7c3706571642a34c3bf0412eb0e0a8ebd532da
FeMess <129093336+FeMess@users.noreply.github.com>
2026-08-15 16:13:59 -0300

    feat: add task listing

--- main.py
+++ main.py
@@ -23,3 +23,25 @@ def add_task(name, description):
             "status": status,
         }
     )
+
+
+def list_tasks(status):
+    status = status.strip().capitalize()
+    filtered_tasks = []
+
+    if status not in ["Pending", "Completed", "All"]:
+        print("Invalid value for status. Try to use [Pending, Completed, All]")
+        return
+
+    for task in tasks:
+        if task["status"] == status or status == "All":
+            filtered_tasks.append(task)
+
+    if not filtered_tasks:
+        print("There are no tasks available for this status.")
+        return
+
+    for task in filtered_tasks:
+        print(
+            f"{task['ID']} | {task['name']} | {task['status']} | {task['description']} "
+        )
